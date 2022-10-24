5aa18126692c74231f63d4fbacd2f83830ccb18a
Syl8n <magi8520@gmail.com>
2022-10-24 19:40:13 +0900

    잔액 사용 API 구현

--- Account/Account/src/main/java/com/example/account/type/ErrorCode.java
+++ Account/Account/src/main/java/com/example/account/type/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     USER_ACCOUNT_UNMATCHED("사용자와 계좌의 소유주가 다릅니다."),
     ACCOUNT_ALREADY_UNREGISTERED("계좌가 이미 해지되었습니다."),
     ACCOUNT_HAS_BALANCE("잔액이 있는 계좌는 해지할 수 없습니다."),
-    MAX_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다.")
+    MAX_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다."),
+    AMOUNT_EXCEED_BALANCE("거래금액이 잔액을 초과합니다.")
     ;
 
     private final String description;
